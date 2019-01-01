0e9dbeeaadc7ff4865d597064633c9795b3c92d1
miriampl <klikacz.jo@gmail.com>
2019-01-01 19:40:33 +0100

    Comment : Fist changes to install core software on wto, 1 sty 2019, 19:40:33 CET

--- 1-install-core-software-v2.sh
+++ 1-install-core-software-v2.sh
@@ -13,30 +13,26 @@ set -e
 ##################################################################################################################
 
 #Software will use Majaro Normal Repository and AUR - Please turn on AUR before use
-#sudo apt-get install -y dropbox evolution focuswriter geany gimp
-#sudo apt-get install -y inkscape meld
-#sudo apt-get install -y openshot pinta plank ppa-purge radiotray screenruler screenfetch scrot shutter slurm synapse
-#sudo apt-get install -y thunar vlc vnstat   
-##sudo apt-get install -y catfish clementine curl dconf-cli dconf-editor geary gpick
-##sudo apt-get install -y glances gparted grsync hardinfo inkscape meld 
-##sudo apt-get install -y openshot pinta plank ppa-purge radiotray screenruler screenfetch scrot shutter slurm synapse
-##sudo apt-get install -y thunar vlc vnstat htop nload steam keepass2 xdotool
 
 yes | sudo pacman -S vlc clementine curl glances grsync
-yes | sudo pacman -S htop nload xdotool
+yes | sudo pacman -S nload keepass2 xdotool meld speedtest-cli gimp
+yes | sudo pacman -S screenfetch
 
 #Application info:
 # VLC - VLC is a free and open source cross-platform multimedia player and framework that plays most multimedia files as well as DVDs, Audio CDs, VCDs, and various streaming protocols. 
 # clementine - GUI Best music player. This tool supports network music like spotyfie or brodcasts.
 # curl - CLI URL crouler. The curl command allows us to query URLs from the command line https://www.youtube.com/watch?v=WxUVU0b95Oc
 # glances - CLI Monitoring tool https://www.youtube.com/watch?v=ZwhyLD-wquk
+# meld - Meld is a visual diff and merge tool targeted at developers. Meld helps you compare files, directories, and version controlled projects.
 # gparted # Majaro Installed Default # - GUI Partition tool. This is defaut in all linux on Live CD to manage Your disk / partitions but not installed in OS it self https://www.youtube.com/watch?v=O5kh_-6e4kk 
 # catfish # Majaro Has other mec taht i try to use # - GUI Searching tool. This tool is good when You need to find a file on Your disk / partition / network device
 # grsync - GUI moving data around and making simple backups tool https://www.youtube.com/watch?v=6nOojLe_CI0
+# speedtest-cli - Command line interface for testing internet bandwidth using speedtest.net
+# openshot - I'm Not using it - OpenShot is a cross-platform video editor, with support for Linux, Mac, and Windows. 
 
-#operating specific software
+# operating specific software
 
-# sudo apt-get install nemo-compare -y
+
 
 ###############################################################################################
 
